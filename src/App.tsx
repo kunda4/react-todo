08a924a3aa226965d95b7df14b30a86f0f697b38
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-11-01 23:27:33 +0200

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import InputField from "./components/inputField";
 
 const App = () => {
+  const [todo, setTodo] = useState<string>("");
+  console.log(todo);
   return (
     <>
       <div className="App">
         <span className="heading">Taskify</span>
-        <InputField />
+        <InputField todo={todo} setTodo={setTodo} />
       </div>
     </>
   );
--- src/components/inputField.tsx
+++ src/components/inputField.tsx
@@ -1,8 +1,20 @@
-const InputField = () => {
+import "./style.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <div>
       <form className="input">
-        <input type="input" placeholder="Enter a task" className="" />
+        <input
+          type="input"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Enter a task"
+          className="input_box"
+        />
         <button className="input_submit" type="submit">
           Go
         </button>
