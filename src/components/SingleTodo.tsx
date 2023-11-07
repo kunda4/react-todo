dd098e452cf96ac612cd0d747fed711afbcf41a3
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-11-07 17:02:41 +0200

    add done and delete functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,6 @@
 import { Todo } from "../model";
 import { AiFillDelete, AiFillEdit, AiOutlineCheck } from "react-icons/ai";
+import "./style.css";
 
 type Props = {
   todo: Todo;
@@ -8,18 +9,31 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  function handleDone(id: number) {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  }
+  function handleDelete(id: number) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
   return (
-    <form action="" key={todo.id}>
-      <span>{todo.todo}</span>
-
+    <form className="todos__single" key={todo.id}>
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
       <div>
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <AiOutlineCheck />
         </span>
       </div>
